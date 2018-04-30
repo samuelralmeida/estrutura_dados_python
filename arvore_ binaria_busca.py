3854ea77fda794da88278a1028623e28066f5fc7
Samuel <samuelsousa.mg@diarioassociados.com.br>
2018-04-30 09:09:57 -0300

    feat: updated binary serach tree

--- arvore_ binaria_busca.py
+++ arvore_ binaria_busca.py
@@ -1,12 +1,7 @@
-"""
-Dado um nó, todos os elementos a esquerda possuem chave menor; os que estão à direita do nó possuem chave maior
-"""
-
 class Node:
 
     def __init__(self, label):
         self.label = label
-        # apontador para filhos da direita e esquerda
         self.left = None
         self.right = None
 
@@ -19,11 +14,11 @@ class Node:
     def getLeft(self):
         return self.left
 
-    def getRight(self):
-        return self.right
-
     def setLeft(self, left):
         self.left = left
+
+    def getRight(self):
+        return self.right
 
     def setRight(self, right):
         self.right = right
@@ -36,20 +31,23 @@ class BinarySearchTree:
 
     def insert(self, label):
 
-        # cria nó
+        # cria um novo nó
         node = Node(label)
 
         # verifica se a árvore está vazia
         if self.empty():
             self.root = node
         else:
+
             # árvore não vazia, insere recursivamente
+
             dad_node = None
             curr_node = self.root
 
             while True:
 
                 if curr_node != None:
+
                     dad_node = curr_node
 
                     # verifica se vai para esquerda ou direita
@@ -59,27 +57,27 @@ class BinarySearchTree:
                     else:
                         # vai para direita
                         curr_node = curr_node.getRight()
-
                 else:
 
-                    # se curr_node É none, é onde inserir
+                    # se curr_node é None, então encontrou onde inserir
+
                     if node.getLabel() < dad_node.getLabel():
                         dad_node.setLeft(node)
                     else:
                         dad_node.setRight(node)
 
-                    break
+                    break  # sai do loop
 
     def empty(self):
         if self.root == None:
             return True
         return False
 
-    # mostra em pré-ordem (raiz-esquerda-direita)
+    # mostra em pré-ordem (raiz-esq-dir)
     def show(self, curr_node):
 
         if curr_node != None:
-            print('{}'.format(curr_node.getLabel()), end=' ')
+            print('%d' % curr_node.getLabel(), end=' ')
             self.show(curr_node.getLeft())
             self.show(curr_node.getRight())
 
@@ -87,13 +85,24 @@ class BinarySearchTree:
         return self.root
 
     def remove(self, label):
-        """
-        CASO 1 - nó removido não tem filho (chamado de folha também), o nó é retirado
-        CASO 2 - nó removido tem apenas 1 filho, esse filho ocupa o lugar do nó removido
-        CASO 3 - nó removido com 2 filhos, pega o menor elemento da subarvore a direita e substitui o removido
-        """
+        '''
+			3 casos
 
-        dad_node = None
+			Caso 1
+			o nó a ser removido não tem filhos
+			esse caso é simples, basta setar a ligação
+			do pai para None
+
+			Caso 2
+			o nó a ser removido tem somente 1 filho
+			basta colocar o seu filho no lugar dele
+
+			Caso 3
+			o nó a ser removido possui dois filhos
+			basta pegar o menor elemento da subárvore à direita
+		'''
+
+        dad_node = None  # parent
         curr_node = self.root
 
         while curr_node != None:
@@ -101,57 +110,119 @@ class BinarySearchTree:
             # verifica se encontrou o nó a ser removido
             if label == curr_node.getLabel():
 
-                # caso 1
+                # caso 1: o nó a ser removido não possui filhos (nó folha)
                 if curr_node.getLeft() == None and curr_node.getRight() == None:
 
                     # verifica se é a raiz
                     if dad_node == None:
                         self.root = None
                     else:
-                        # verifica se é filho a esquerda ou direita
+                        # verifica se é filho à esquerda ou à direita
                         if dad_node.getLeft() == curr_node:
-                            # filho a esquerda
                             dad_node.setLeft(None)
                         elif dad_node.getRight() == curr_node:
-                            # filho a direita
-                            dad_node.setRigth(None)
+                            dad_node.setRight(None)
 
-                # caso 2
-                elif (curr_node.getLeft() is None and curr_node.getRight() is not None) or \
-                     (curr_node.getLeft() is not None and curr_node.getRight() is None):
+                # caso 2: o nó a ser removido possui somente um filho
+                elif (curr_node.getLeft() == None and curr_node.getRight() != None) or \
+                        (curr_node.getLeft() != None and curr_node.getRight() == None):
 
-                    # verifica se o nó a ser removido é raiz
+                    # verifica se o nó a ser removido é a raiz
                     if dad_node == None:
-                        # verifica se o filho de curr_node é filho a esquerda
+                        # verifica se o filho de curr_node é filho à esquerda
                         if curr_node.getLeft() != None:
                             self.root = curr_node.getLeft()
-                        else:
-                            # filho é a direita
+                        else:  # senão o filho de curr_node é filho à direita
                             self.root = curr_node.getRight()
-                    # se não for raiz
                     else:
                         # verifica se o filho de curr_node é filho à esquerda
                         if curr_node.getLeft() != None:
-                            # verifica se curr_node é filho a esquerda
+                            # verifica se curr_node é filho à esquerda
                             if dad_node.getLeft() and dad_node.getLeft().getLabel() == curr_node.getLabel():
                                 dad_node.setLeft(curr_node.getLeft())
-                            else:
-                                # curr_node é filho a direita
+                            else:  # senão curr_node é filho à direita
                                 dad_node.setRight(curr_node.getLeft())
-                        else:
-                            # filho de curr_node é filho à esquerda
-                            # verifica se curr_node é filho a esquerda
+                        else:  # senão o filho de curr_node é filho à direita
+                            # verifica se curr_node é filho à esquerda
                             if dad_node.getLeft() and dad_node.getLeft().getLabel() == curr_node.getLabel():
                                 dad_node.setLeft(curr_node.getRight())
+                            else:  # senão curr_node é filho à direita
+                                dad_node.setRight(curr_node.getRight())
+
+                # caso 3: o nó a ser removido possui dois filhos
+                # pega-se o menor elemento da subárvore à direita
+                elif curr_node.getLeft() != None and curr_node.getRight() != None:
+
+                    dad_smaller_node = curr_node
+                    smaller_node = curr_node.getRight()
+                    next_smaller = curr_node.getRight().getLeft()
+
+                    while next_smaller != None:
+                        dad_smaller_node = smaller_node
+                        smaller_node = next_smaller
+                        next_smaller = smaller_node.getLeft()
+
+                    # verifica se o nó a ser removido é a raiz
+                    if dad_node == None:
+
+                        # Caso especial: o nó que vai ser a nova raiz é filho da raiz
+                        if self.root.getRight().getLabel() == smaller_node.getLabel():
+                            smaller_node.setLeft(self.root.getLeft())
+                        else:
+
+                            '''
+								verifica se o smaller_node é filho à esquerda ou à direita
+								para setar para None o smaller_node
+							'''
+                            if dad_smaller_node.getLeft() and \
+                                    dad_smaller_node.getLeft().getLabel() == smaller_node.getLabel():
+                                dad_smaller_node.setLeft(None)
                             else:
-                                # curr_node é filho a direita
-                                dad_node.setRight(curr_node.Right())
+                                dad_smaller_node.setRight(None)
 
-                # caso 3
+                            # seta os filhos à direita e esquerda de smaller_node
+                            smaller_node.setLeft(curr_node.getLeft())
+                            smaller_node.setRight(curr_node.getRight())
 
+                        # faz com que o smaller_node seja a raiz
+                        self.root = smaller_node
 
+                    else:
 
+                        '''
+							verifica se curr_node é filho à esquerda ou à direita
+							para setar o smaller_node como filho do pai do curr_node (dad_node)
+						'''
+                        if dad_node.getLeft() and dad_node.getLeft().getLabel() == curr_node.getLabel():
+                            dad_node.setLeft(smaller_node)
+                        else:
+                            dad_node.setRight(smaller_node)
 
+                        '''
+							verifica se o smaller_node é filho à esquerda ou à direita
+							para setar para None o smaller_node
+						'''
+                        if dad_smaller_node.getLeft() and \
+                                dad_smaller_node.getLeft().getLabel() == smaller_node.getLabel():
+                            dad_smaller_node.setLeft(None)
+                        else:
+                            dad_smaller_node.setRight(None)
+
+                        # seta os filhos à direita e esquerda de smaller_node
+                        smaller_node.setLeft(curr_node.getLeft())
+                        smaller_node.setRight(curr_node.getRight())
+
+                break  # sai do loop
+
+            dad_node = curr_node
+
+            # verifica se vai para esquerda ou direita
+            if label < curr_node.getLabel():
+                # vai para esquerda
+                curr_node = curr_node.getLeft()
+            else:
+                # vai para direita
+                curr_node = curr_node.getRight()
 
 
 t = BinarySearchTree()
@@ -164,5 +235,8 @@ t.insert(7)
 t.insert(10)
 t.insert(14)
 t.insert(13)
+t.insert(9)
+
+t.remove(13)
 
 t.show(t.getRoot())
